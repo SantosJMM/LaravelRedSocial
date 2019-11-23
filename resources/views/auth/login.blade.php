00031de5733f98fdc87b89497f07b396954f3360
Santos J. Marte M <alejandro.marte.07@gmail.com>
2019-11-23 18:42:48 -0400

    stilos para el form de login. agrego el enlace login solo para los usuario no logueado.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,26 @@
-<form action="{{ route('login') }}" method="post">
-    @csrf
-    <input type="email" name="email">
-    <input type="password" name="password">
-    <button id="login-btn">Login</button>
-</form>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mx-auto">
+                <div class="card border-0 bg-light px-4 py-2">
+                    <form action="{{ route('login') }}" method="post">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input class="form-control border-0" type="email" name="email" id="email" placeholder="Tu nombre...">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Contraseña:</label>
+                                <input class="form-control border-0" type="password" name="password" id="password" placeholder="Tu contraseña...">
+                            </div>
+                            <button class="btn btn-primary btn-block" id="login-btn">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light navbar-socialapp">
         <div class="container">
-            <a class="navbar-brand" href="#">SocialApp</a>
+            <a class="navbar-brand" href="{{ route('home') }}">SocialApp</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -29,18 +29,23 @@
                 </ul>
 
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
+                    @guest
+                        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+                    @else
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                               aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a onclick="document.getElementById('logout').submit()" class="dropdown-item" href="#">Cerrar sesión</a>
+                                <form action="{{ route('logout') }}" method="post" id="logout" style="display: none;">@csrf</form>
+                            </div>
+                        </li>
+                    @endguest
                 </ul>
             </div>
         </div>
