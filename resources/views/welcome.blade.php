29249fdec656a084c2a864ba0151d23427182810
Santos J. Marte M <alejandro.marte.07@gmail.com>
2019-11-23 18:10:44 -0400

    creacion del layout, instalacion de laravel ui y bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,21 @@
-<form action="{{ route('statuses.store') }}" method="post">
-    @csrf
-    <label for="body">Body</label>
-    <textarea name="body" id="body" cols="30" rows="10"></textarea>
-    <button id="create-status">Publicar estado</button>
-</form>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-8 mx-auto">
+                <div class="card border-0 bg-light">
+                    <form action="{{ route('statuses.store') }}" method="post">
+                        @csrf
+                        <div class="card-body">
+                            <textarea class="form-control border-0 bg-light" name="body" id="body" placeholder="¿Qué estás pensando Alex?"></textarea>
+                        </div>
+                        <div class="card-footer">
+                            <button class="btn btn-primary" id="create-status">Publicar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
