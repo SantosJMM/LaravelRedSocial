21809e1ce9ae17e861691e571df70a95d2d1ee2c
Santos J. Marte M <alejandro.marte.07@gmail.com>
2019-11-24 19:32:08 -0400

    test de validation para register controller

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Hash;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
@@ -15,14 +16,7 @@ class RegistrationTest extends TestCase
     public function users_can_register()
     {
         $this->withoutExceptionHandling();
-        $userData = [
-            'name' => 'alex07',
-            'first_name' => 'Alex',
-            'last_name' => 'Marte',
-            'email' => 'alex@email.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ];
+        $userData = $this->userValidData();
 
         $response = $this->post(route('register'), $userData);
         $response->assertRedirect('/');
@@ -35,5 +29,187 @@ class RegistrationTest extends TestCase
         $this->assertTrue(
             Hash::check('password', User::first()->password)
         );
+    }
+
+    /** @test */
+    public function the_name_is_required()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['name' => null])
+        )->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function the_name_must_be_a_string()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['name' => 1231])
+        )->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function the_name_may_not_be_greater_than_60_characters()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['name' => Str::random(61)])
+        )->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function the_first_name_is_required()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['first_name' => null])
+        )->assertSessionHasErrors('first_name');
+    }
+
+    /** @test */
+    public function the_first_name_must_be_a_string()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['first_name' => 1231])
+        )->assertSessionHasErrors('first_name');
+    }
+
+    /** @test */
+    public function the_first_name_may_not_be_greater_than_255_characters()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['first_name' => Str::random(256)])
+        )->assertSessionHasErrors('first_name');
+    }
+
+    /** @test */
+    public function the_last_name_is_required()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['last_name' => null])
+        )->assertSessionHasErrors('last_name');
+    }
+
+    /** @test */
+    public function the_last_name_must_be_a_string()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['last_name' => 1231])
+        )->assertSessionHasErrors('last_name');
+    }
+
+    /** @test */
+    public function the_last_name_may_not_be_greater_than_255_characters()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['last_name' => Str::random(256)])
+        )->assertSessionHasErrors('last_name');
+    }
+
+    /** @test */
+    public function the_email_is_required()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['email' => null])
+        )->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function the_email_must_be_a_string()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['email' => 1231])
+        )->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function the_email_may_not_be_greater_than_255_characters()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['email' => Str::random(256)])
+        )->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function the_email_must_be_a_valid_email_address()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['email' => 'ale'])
+        )->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function the_email_must_be_unique()
+    {
+        factory(User::class)->create(['email' => 'alex@email.com']);
+        $this->post(
+            route('register'),
+            $this->userValidData(['email' => 'alex@email.com'])
+        )->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function the_password_is_required()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['password' => null])
+        )->assertSessionHasErrors('password');
+    }
+
+    /** @test */
+    public function the_password_must_be_a_string()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['password' => 1231])
+        )->assertSessionHasErrors('password');
+    }
+
+    /** @test */
+    public function the_password_must_be_at_least_8_characters()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData(['password' => Str::random(7)])
+        )->assertSessionHasErrors('password');
+    }
+
+    /** @test */
+    public function the_password_must_be_confirmed()
+    {
+        $this->post(
+            route('register'),
+            $this->userValidData([
+                'password' => Str::random(8),
+                'password_confirmation' => null
+            ])
+        )->assertSessionHasErrors('password');
+    }
+
+    /**
+     * @param array $overrides
+     * @return array
+     */
+    public function userValidData($overrides = []): array
+    {
+        return array_merge([
+            'name' => 'alex07',
+            'first_name' => 'Alex',
+            'last_name' => 'Marte',
+            'email' => 'alex@email.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ], $overrides);
     }
 }
