697594a03b7e2cd8a8b95791c03bbfda6381cfd4
Santos J. Marte M <alejandro.marte.07@gmail.com>
2019-11-27 04:06:22 -0400

    test de regresion

--- app/Models/Friendship.php
+++ app/Models/Friendship.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use App\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * @method static create(array $array)
  * @method static where(array $array)
  * @method static firstOrCreate(array $array)
+ * @method static betweenUsers($sender, $recipient)
  */
 class Friendship extends Model
 {
@@ -22,5 +24,24 @@ class Friendship extends Model
     public function recipient()
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Scope a query to only include popular users.
+     *
+     * @param Builder $query
+     * @param User $sender
+     * @param User $recipient
+     * @return Builder
+     */
+    public function scopeBetweenUsers($query, $sender, $recipient)
+    {
+        return  $query->where([
+            ['sender_id', $sender->id],
+            ['recipient_id', $recipient->id],
+        ])->orWhere([
+            ['sender_id', '=', $recipient->id],
+            ['recipient_id', '=', $sender->id],
+        ]);
     }
 }
